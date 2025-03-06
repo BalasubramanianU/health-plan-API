7da88997af977544b913001e0dcff37c02d9ef5a
Balasubramanian U <ubalasubramanian03@gmail.com>
2025-03-06 17:25:24 -0500

    Demo two - OAuth 2.0 implementation with Google Auth

---
+++ middlewares/auth.js
@@ -0,0 +1,75 @@
+const jwt = require("jsonwebtoken");
+const jwkToPem = require("jwk-to-pem");
+const ApiError = require("../utils/ApiError");
+
+/**
+ * Fetches Google's public JWK keys
+ * @returns {Promise<Object>} Google's public keys
+ */
+const getGooglePublicKeys = async () => {
+  const response = await fetch("https://www.googleapis.com/oauth2/v3/certs");
+  if (!response.ok) {
+    // change to proper error handling
+    throw new Error(`HTTP error! Status: ${response.status}`);
+  }
+  const data = await response.json();
+  return data.keys;
+};
+
+// Middleware to verify Google OAuth2.0 Bearer JWT Token
+const verifyToken = async (req, res, next) => {
+  if (!req.headers["authorization"]) {
+    // return res.status(400).send("Authorization header missing");
+    return next(ApiError.badRequest());
+  }
+
+  const authHeaderParts = req.headers["authorization"].split(" ");
+  if (authHeaderParts[0] !== "Bearer" || !authHeaderParts[1]) {
+    // return res.status(400).send("Invalid Bearer Token");
+    return next(ApiError.badRequest());
+  }
+
+  const idToken = authHeaderParts[1];
+
+  try {
+    // Decode JWT header
+    const decodedHeader = jwt.decode(idToken, { complete: true });
+    if (!decodedHeader) {
+      //   return res.status(400).send("Invalid JWT token");
+      return next(ApiError.badRequest());
+    }
+
+    const kid = decodedHeader.header.kid;
+    const alg = decodedHeader.header.alg;
+
+    // Fetch Google's public keys
+    const keys = await getGooglePublicKeys();
+
+    // Find the correct key
+    const key = keys.find((k) => k.kid === kid);
+    if (!key) {
+      //   return res.status(400).send("Matching public key not found");
+      return next(ApiError.badRequest());
+    }
+
+    // Convert JWK to PEM
+    const publicKey = jwkToPem(key);
+
+    // Verify JWT signature and audience
+    const payload = jwt.verify(idToken, publicKey, {
+      algorithms: [alg],
+      //   make the client id here to env variable
+      audience:
+        "632426186384-tgm3tmmotmpfss1p7rb52fc5pg4lesv5.apps.googleusercontent.com",
+    });
+
+    // req.user = payload; // Attach user info to the request
+    next();
+  } catch (err) {
+    console.error(err);
+    // res.status(401).send("Unauthorized");
+    return next(ApiError.unauthorized());
+  }
+};
+
+module.exports = verifyToken;
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const healthPlan = require("./healthPlan");
 const { pageNotFound } = require("../middlewares/utils");
 const ApiError = require("../utils/ApiError");
+const verifyToken = require("../middlewares/auth");
 
 module.exports = (app) => {
   app.use(express.json(), (err, req, res, next) => {
@@ -12,7 +13,7 @@ module.exports = (app) => {
 
   const version = process.env.API_VERSION || "v1";
 
-  app.use(`/${version}/plan`, healthPlan);
+  app.use(`/${version}/plan`, verifyToken, healthPlan);
 
   app.use("*", pageNotFound);
 };
